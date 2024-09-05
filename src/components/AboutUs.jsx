23c476739c6c43cc9f944e80ecca546dc955957d
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-09-05 12:00:02 +0530

    final commit

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutUs = () => {
+  return (
+    <div>
+      <p>This application demonstrates the basic routing demo!</p>
+    </div>
+  )
+}
+
+export default AboutUs
